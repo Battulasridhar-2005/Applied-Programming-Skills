9f4fa26bca553a3fd41426ba29875427f75d1117
BATTULA SRIDHAR <vtu26553@veltech.edu.in>
2026-04-29 17:37:08 +0530

    Time: 2 ms (70.96%), Space: 55.7 MB (32.16%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,22 +1,10 @@
 class Solution {
     public int majorityElement(int[] nums) {
-
-        int candidate = 0;
-        int count = 0;
-
+        int count = 0, candidate = 0;
         for (int num : nums) {
-
-            if (count == 0) {
-                candidate = num;
-            }
-
-            if (num == candidate) {
-                count++;
-            } else {
-                count--;
-            }
+            if (count == 0) candidate = num;
+            count += (num == candidate) ? 1 : -1;
         }
-
         return candidate;
     }
 }
