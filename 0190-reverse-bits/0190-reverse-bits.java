0214cf679328f866eb3dc97cb65238c333454c65
BATTULA SRIDHAR <vtu26553@veltech.edu.in>
2026-04-29 17:38:20 +0530

    Time: 1 ms (60.53%), Space: 42.6 MB (34.6%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,17 +1,11 @@
-class Solution {
+public class Solution {
     public int reverseBits(int n) {
-
         int result = 0;
-
         for (int i = 0; i < 32; i++) {
-
-            int bit = n & 1;
-
-            result = (result << 1) | bit;
-
-            n = n >> 1;
+            result <<= 1;
+            result |= (n & 1);
+            n >>= 1;
         }
-
         return result;
     }
 }
